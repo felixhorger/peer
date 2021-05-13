2debc3485494d4db0610596b02dc366db56bb588
Felix Horger <felix.horger@kcl.ac.uk>
2021-05-13 14:00:12 +0100

    [celsus/bibtex] Added slash to regex for DOI.

--- celsus/bibtex.py
+++ celsus/bibtex.py
@@ -57,7 +57,7 @@ empty_bib_article = (
 re_author = re.compile(r'\s+author[\s={\']+([\w\-\s\.,]+?)(\s+and\s+|\s*[\'}]).*')
 re_year = re.compile(r'\s*year[\s={\']+([0-9]+).*')
 re_citation_key = re.compile(r'\s*@[a-zA-Z]+{(.*?),.*')
-re_doi = re.compile(r'[0-9a-zA-Z-]+\.[0-9a-zA-Z-]+/[-_;:\.<>()0-9a-zA-Z]+$')
+re_doi = re.compile(r'[0-9a-zA-Z-]+\.[0-9a-zA-Z-]+/[-_;:\.<>/()0-9a-zA-Z]+$')
 re_arxiv = re.compile(r'[0-9]{2}(0[1-9]|1[0-2])\.[0-9]{5}(v[1-9]{1}|)$')
 re_doi_in_text = re.compile(
 	'(^|\s+)('
